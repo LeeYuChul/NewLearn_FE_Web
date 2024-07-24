da04dc767957ef4c7893fd305f7a0c46fa82f86a
leeYuChul <yoochul89@naver.com>
2024-07-24 17:13:47 +0900

    0724_5차 누락

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1786973579"
+    serviceWorkerVersion: "3331825650"
   }
 });
