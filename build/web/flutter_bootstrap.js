e2d14944679c24587321d32c5f1423a31247a352
leeYuChul <yoochul89@naver.com>
2024-07-31 20:28:28 +0900

    [CHORE] 웹 빌드

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1500762988"
+    serviceWorkerVersion: "4150760226"
   }
 });
